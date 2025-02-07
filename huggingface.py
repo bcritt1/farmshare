6507eb9564578454156addb5ff2ae4195bced949
bcritt1 <bcritt@stanford.edu>
2025-02-07 14:03:20 -0800

    update 2025

--- huggingface.py
+++ huggingface.py
@@ -1,7 +1,9 @@
 # Import packages
 import os
 import json
-
+from transformers import pipeline
+from transformers.models.roberta import RobertaTokenizer, RobertaForTokenClassification
+import torch
 # Read in corpus
 user = os.getenv('USER')
 corpusdir = '/farmshare/home/groups/srcc/cesta_workshop/corpus/'
@@ -12,16 +14,13 @@ for infile in os.listdir(corpusdir):
         corpus.append(fin.read())
 
 # Import language models and pipeline elements
-from transformers import AutoTokenizer, AutoModelForTokenClassification
-tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-
+tokenizer = RobertaTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+model = RobertaForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
 
 # Process corpus
-from transformers import pipeline
 nlp = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strategy="simple")
 entities = nlp(corpus)
 
 # Export data to json
-with open('/home/{}/outputs/dataRoberta.json'.format(user), 'w', encoding='utf-8') as f:
+with open('/scratch/users/{}/outputs/dataRoberta.json'.format(user), 'w', encoding='utf-8') as f:
     json.dump(str(entities), f, ensure_ascii=False, indent=4)
